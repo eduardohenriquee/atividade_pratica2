ceb276d3b6d9e14c60215e9138ad1cbf1b93f284
eduardohenriquee <48135920+eduardohenriquee@users.noreply.github.com>
2019-07-09 10:11:37 -0300

    Add files via upload

---
+++ exerc2php.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Exercício 1 - Selecione o seu carro</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+<div class="quadrado">
+<h1> Cor selecionada: </h1><br>
+
+<?php 
+    $r= $_POST["r"];
+    $g= $_POST["g"];
+    $b= $_POST["b"];
+    
+    if ($r == null)
+        {$r=0;}
+    if ($g == null)
+    {$g=0;}
+    if ($b == null)
+    {$b=0;}
+    
+    
+    
+    ?>
+
+<p>
+    R: <?php echo $r;?>
+    G: <?php echo $g;?>
+    B: <?php echo $b;?>
+</p>
+
+<div style="background-color: rgb(<?php print "$r, $g, $b";?>); position:relative; height:150px; align:center;"></div> 
+
+</div>
+
+</body>
+
+</html>
